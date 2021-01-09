863a7f04a16a0a3ecbab4fd4ae1fd96575f0c481
Kirill-Main <kir20-01@ya.ru>
2021-01-10 02:54:00 +0300

    selection sort has been changed

--- Ruby/selection_sort.rb
+++ Ruby/selection_sort.rb
@@ -6,13 +6,9 @@ def selection_sort(array = nil)
   length = array.length
   new_array = Array.new array
   (length - 1).times do |i|
-    min_index = i
-
-    (i + 1).upto(length - 1) do |j|
-      min_index = j if new_array[min_index] > new_array[j]
+    if element = new_array.each_with_index.select { |el| el[1] > i && el[0] < new_array[i] }.min
+      new_array[element[1]], new_array[i] = new_array[i], new_array[element[1]]
     end
-
-    new_array[min_index], new_array[i] = new_array[i], new_array[min_index] if min_index != i
   end
   new_array
 end
